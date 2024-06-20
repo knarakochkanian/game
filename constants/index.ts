0a7c5fb7600e22c424e957de11278e523ed81ae7
kochkanianknara <knarakochkanian@gmail.com>
2024-06-21 02:23:14 +0400

    (fix ) data industry

--- constants/index.ts
+++ constants/index.ts
@@ -77,14 +77,23 @@ export const stateSystemOff =
   'Пуск задачи невозможен. Текущая задача будет сохранена и доступна к запуску, когда система подключится к защищенной сети.';
 
 //sector names
-export const IT_SECTOR = 'IT-сектор';
-export const ENERGY = 'энергетика';
-export const FINANCE = 'финансы';
-export const RETAIL = 'розничная торговля';
-export const INDUSTRY = 'промышленность';
-export const LOG_AND_TRANSPORT = 'логистика и траспорт';
-export const UNIVERSE = 'космос';
-export const GOV_INFOSTRUCTURES = 'гос. инфраструктура';
+export const obrazovaniye = 'образование';
+export const seti_svyazi = 'сети связи';
+export const energetika = 'энергетика';
+export const turizm = 'туризм';
+export const SMI = 'СМИ';
+export const finansovaya_sistema = 'финансовая система';
+export const transportnaya_sistema = 'транспортная система';
+export const stroitelstvo = 'строительство';
+export const torgovlya = 'торговля';
+export const promyshlennost = 'промышленность';
+export const kosmos = 'космос';
+export const zdravookhraneniye = 'здравоохранение';
+export const dobycha_polez_iskopayemykh = 'добыча полезных ископаемых';
+export const gos_infrastruktura = 'государственная инфраструктура';
+export const vooruzhennyye_sily = 'вооруженные силы';
+export const Vodosnabzheniye = 'водоснабжение';
+export const APK = 'АПК';
 export const VPK = 'ВПК';
 export const COMPANY_TOP_CAPITALIZATION = 'Топ капитализаций компаний';
 
