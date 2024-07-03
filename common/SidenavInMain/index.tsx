d8ce2e7f9880f7c9f6d87812e99599466c4f25b3
BritishEllie <visnevskijan@gmail.com>
2024-07-04 00:25:45 +0500

    disable accept button

--- common/SidenavInMain/index.tsx
+++ common/SidenavInMain/index.tsx
@@ -302,6 +302,7 @@ function SidenavInMain({
                   href={delayedTime && delayedDate ? '/queue' : '/summary'}
                   onClick={onSetCurrentAction}
                   ref={confirmButtonRef}
+                  style={{ pointerEvents: "none" }}
                 >
                   <span
                     className="Lead"
