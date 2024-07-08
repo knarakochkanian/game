92c6ba342e1ec056bdbf570989e702d7fee565cb
Andrey Khitryy <andrey.khitryy@gmail.com>
2024-07-08 15:44:45 +0300

    Cleanup password only on app start

--- android/app/src/main/java/com/raspberry/MainActivity.java
+++ android/app/src/main/java/com/raspberry/MainActivity.java
@@ -1,5 +1,17 @@
 package com.raspberry;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    private static final String CLEAR_PASS_JS = "window.localStorage.removeItem('isPasswordPassed')";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getBridge().eval(CLEAR_PASS_JS, value -> {
+            Log.v("MainActivity", "Device will be locked");
+        });
+    }
+}
