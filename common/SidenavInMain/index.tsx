caa71910d187148c0d8b374b8918800626d607ab
Aghasy8888 <aghasghazaryan8888@gmail.com>
2024-07-08 17:12:10 +0400

    disabled submit button

--- common/SidenavInMain/index.tsx
+++ common/SidenavInMain/index.tsx
@@ -328,8 +328,7 @@ function SidenavInMain({
           {numberOfSelectedSectors !== null &&
             damageLevel &&
             selectedCountries.length !== 0 &&
-            // !pingFailed &&
-             (
+            !pingFailed && (
               <div className={styles.sidenavAddConfirm}>
                 <Image
                   src={
@@ -354,7 +353,7 @@ function SidenavInMain({
                   href={delayedTime && delayedDate ? '/queue' : '/summary'}
                   onClick={onSetCurrentAction}
                   ref={confirmButtonRef}
-                  // style={{ pointerEvents: 'none' }}
+                  style={{ pointerEvents: 'none' }}
                 >
                   <span
                     className="Lead"
