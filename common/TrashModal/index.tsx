53738fa48b2357912643617f143cc73c48fd71ad
BritishEllie <visnevskijan@gmail.com>
2024-07-12 00:13:26 +0500

    fix trash modal

--- common/TrashModal/index.tsx
+++ common/TrashModal/index.tsx
@@ -12,12 +12,12 @@ type TTrashModalProps = {
 };
 
 const TrashModal = ({
-  closeModal,
-  name,
-  trashCallBack,
-  trashModalOpen,
-  fromCountDown,
-}: TTrashModalProps) => {
+                      closeModal,
+                      name,
+                      trashCallBack,
+                      trashModalOpen,
+                      fromCountDown,
+                    }: TTrashModalProps) => {
   const webSocketContext = useWebSocket();
 
   if (!webSocketContext) {
@@ -48,55 +48,60 @@ const TrashModal = ({
   const handleCancelInCountDown = () => {
     if (socket && socket.readyState === WebSocket.OPEN) {
       socket.send('nope');
+      setTimeout(() => {
+        if (socket.readyState === WebSocket.OPEN) {
+          socket.send('ping');
+        }
+      }, 1000);
     }
     closeModal();
   };
 
   return (
-    <Modal
-      name={name}
-      isOpen={trashModalOpen}
-      onClose={closeModal}
-      counter={2}
-      sx={{ left: '1%', top: '12% !important', position: 'absolute' }}
-    >
-      {fromCountDown ? (
-        <p> Вы уверены что хотите отменить запуск?</p>
-      ) : (
-        <p> Вы уверены что хотите удалить задачу?</p>
-      )}
-      <div className="ModalButtons">
+      <Modal
+          name={name}
+          isOpen={trashModalOpen}
+          onClose={closeModal}
+          counter={2}
+          sx={{ left: '1%', top: '12% !important', position: 'absolute' }}
+      >
         {fromCountDown ? (
-          <Link
-            style={{
-              textDecoration: 'none',
-              color: 'white',
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-            href={'launch-stopped'}
-            className="ModalButton1"
-            onClick={handleDeleteInCountDown}
-          >
-            ДА
-          </Link>
+            <p> Вы уверены что хотите отменить запуск?</p>
         ) : (
-          <button className="ModalButton1" onClick={handleDelete}>
-            удалить
-          </button>
+            <p> Вы уверены что хотите удалить задачу?</p>
         )}
-        {fromCountDown ? (
-          <button className="SecondarySmall" onClick={closeModal}>
-            <span className="TypoBodyBigLink">НЕТ</span>
-          </button>
-        ) : (
-          <button className="SecondarySmall" onClick={handleCancelInCountDown}>
-            <span className="TypoBodyBigLink">отмена</span>
-          </button>
-        )}
-      </div>
-    </Modal>
+        <div className="ModalButtons">
+          {fromCountDown ? (
+              <Link
+                  style={{
+                    textDecoration: 'none',
+                    color: 'white',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                  href={'launch-stopped'}
+                  className="ModalButton1"
+                  onClick={handleDeleteInCountDown}
+              >
+                ДА
+              </Link>
+          ) : (
+              <button className="ModalButton1" onClick={handleDelete}>
+                удалить
+              </button>
+          )}
+          {fromCountDown ? (
+              <button className="SecondarySmall" onClick={handleCancelInCountDown}>
+                <span className="TypoBodyBigLink">НЕТ</span>
+              </button>
+          ) : (
+              <button className="SecondarySmall" onClick={handleCancelInCountDown}>
+                <span className="TypoBodyBigLink">отмена</span>
+              </button>
+          )}
+        </div>
+      </Modal>
   );
 };
 
