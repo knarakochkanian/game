b5d1b889cfb34d3e5a6f71ee5fa799e6be9a3c4d
BritishEllie <visnevskijan@gmail.com>
2024-08-20 17:42:36 +0500

    fix main activity

--- android/app/src/main/java/com/raspberry/MainActivity.java
+++ android/app/src/main/java/com/raspberry/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends BridgeActivity {
 //         );
 
        ntpManager = new NTPManager(
+               this,
                jsBridge,
                "time.google.com",
                "google.com"
